24bb0f3f14601b2a2aa47454879c27850c5962a3
Johnelle Walker <johnellewalker@gmail.com>
2023-10-29 18:09:58 -0600

    complete Objective2 Labs 1, 2, & 3

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Johnelle Walker";
+    int age = 33;
+    String state = "California";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+    
+  }
+}
---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,14 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+
+    int year = 1990;
+    String month = "April";
+    boolean isWeekend = false;
+
+    System.out.println(year);
+    System.out.println(month);
+    System.out.println(isWeekend);
+
+
+  }
+}
