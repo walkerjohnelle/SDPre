8a9b084192f90c8caf643de737c49bcb92efeef9
Johnelle Walker <johnellewalker@gmail.com>
2023-10-30 13:53:32 -0600

    complete Objective7 Labs 1, 2, 3, 4, & 5

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+	public static void main(String[] args) {
+		int count = 1;
+		int sum = 0;
+
+		while (count <= 20) {
+			sum += count;
+			count++;
+		}
+		System.out.println(sum);
+	}
+}
