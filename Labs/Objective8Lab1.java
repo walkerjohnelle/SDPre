6931bf1e41892a02795190b7152d5637f3b76d01
Johnelle Walker <johnellewalker@gmail.com>
2023-10-30 14:32:46 -0600

    complete Objective 8 Labs 1, 2, & 3

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,13 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    int i;
+
+    for (i = 1; i <= 10; i++) { 
+      System.out.println(i);
+    }
+
+    for (i = 10; i >= 1; i--) {
+      System.out.println(i);
+    }
+  }
+}
---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,14 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+
+    for (counter = 1; counter <=20; counter++) {
+      if (counter % 2 == 0) {
+        System.out.println(counter + " is even");
+      } else {
+        System.out.println(counter + " is odd");
+      }
+    }
+    
+  }
+}
