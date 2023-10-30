4f65490d363111050f9c9028791bbfb738148e64
Johnelle Walker <johnellewalker@gmail.com>
2023-10-30 09:37:38 -0600

    complete objective 4 labs 1, 2, & 3

---
+++ Labs/Objective4Lab1.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Objective4Lab1 {
+  public static void main(String[] args) {
+
+    Scanner keyboard = new Scanner(System.in);
+
+    String fname = "";
+    String lname = "";
+    String favoriteAnimal = "";
+    String favoriteFood = "";
+    String favoriteSong = "";
+
+     System.out.println("What is your first name?");
+    fname = keyboard.nextLine();
+    
+    System.out.println("What is your last name?");
+    lname = keyboard.nextLine();
+    
+    System.out.println("What is your favorite animal?");
+    favoriteAnimal = keyboard.nextLine();
+    
+    System.out.println("What is your favorite food?");
+    favoriteFood = keyboard.nextLine();
+    
+    System.out.println("What is your favorite song?");
+    favoriteSong = keyboard.nextLine();
+
+    System.out.println("My name is " + fname + " " + lname + ".");
+    System.out.println("My favorite animal is the " + favoriteAnimal + ".");
+    System.out.println("My favorite food is " + favoriteFood + ".");
+    System.out.println("My favorite song is " + favoriteSong + ".");
+
+  }
+}
---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Objective4Lab3 {
+  public static void main(String[] args) {
+    int birthYear, age, currentYear;
+
+    birthYear = 0;
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("How old are you?");
+
+    age = input.nextInt();
+    currentYear = java.time.Year.now().getValue(); // Get the current year
+    birthYear = currentYear - age;
+
+    System.out.println("You were born in " + birthYear);
+    input.close();
+  }
+}
