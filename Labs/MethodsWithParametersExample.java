3ae74ea3ed369a08ebfbe29b90cf018571149ef8
Johnelle Walker <johnellewalker@gmail.com>
2023-10-31 09:45:36 -0600

    complete MethodsWithParametersExample, Objective9 Labs 1, 2, 3, & 4

---
+++ Labs/MethodsWithParametersExample.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MethodsWithParametersExample {
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		double num1, num2;
+		int quantity;
+		System.out.print("Please give me one decimal number: ");
+		num1 = kb.nextDouble();
+		System.out.print("Please give me another decimal number: ");
+		num2 = kb.nextDouble();
+
+		double answer = findSum(num1, num2);
+		System.out.println(num1 + " + " + num2 + " = " + answer);
+	}
+
+	public static double findSum(double x, double y) {
+		double sum = x + y;
+		return sum;
+	}
+	
+}
